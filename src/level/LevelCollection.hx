3abe1ff682a0ea077877f4441b4507920bfbe533
Bart Veldstra <bart.veldstra@gmail.com>
2013-08-25 22:11:02 +0200

    Fixed bug where finishing the last level would crash the game

--- src/level/LevelCollection.hx
+++ src/level/LevelCollection.hx
@@ -4,6 +4,6 @@ import openfl.Assets;
 class LevelCollection {
     public static function getAllLevels():Array<String> {
         var levels = Assets.getText("levels/_list.txt");
-        return levels.split("\n");
+        return StringTools.trim(levels).split("\n");
     }
 }
